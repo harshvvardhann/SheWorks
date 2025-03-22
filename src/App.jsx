ae615962e9f8edb58df1c344ca7940bba8a3f2dd
Neel2102 <neelsutariya21@gmail.com>
2025-03-23 00:16:49 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Entrepreneurs from '../src/components/Entrepreneurs/Entrepreneurs';
 import WomenEntrepreneurForm from './components/WomenEntrepreneurs/WomenEntrepreneurForm';
 import BusinessList from './components/BusinessList/BusinessList';
+import FinancialLearningPlatform from './components/financiallearningplatform/financiallearningplatform';
+import AboutUs from './components/aboutus/aboutus';
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
                     <Route path="/entrepreneur-form" element={<WomenEntrepreneurForm />} />
                     <Route path="/entrepreneurs" element={<Entrepreneurs />} />
                     <Route path="/business-list" element={<BusinessList />} />
+                    <Route path="/financiallearningplatform" element={<FinancialLearningPlatform />} />
+                    <Route path="/aboutus" element={<AboutUs />} />
                 </Routes>
             </div>
         </Router>
